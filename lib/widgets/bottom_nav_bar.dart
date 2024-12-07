bbc73de8a51ac9fd8631a561d2f6f561f727398a
habibiezkrillh <zikrillahhabibie@gmail.com>
2024-12-07 09:22:41 +0800

    bottom navigation test

--- lib/widgets/bottom_nav_bar.dart
+++ lib/widgets/bottom_nav_bar.dart
@@ -3,34 +3,41 @@ import 'package:flutter/material.dart';
 class BottomNavBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return BottomNavigationBar(
-      items: const [
-        BottomNavigationBarItem(
-          icon: Icon(Icons.info),
-          label: 'About Me',
+    return BottomAppBar(
+      shape: CircularNotchedRectangle(),
+      notchMargin: 8.0,
+      child: Container(
+        height: 60.0,
+        padding: EdgeInsets.symmetric(horizontal: 20.0),
+        decoration: BoxDecoration(
+          color: Colors.blue[200],
+          borderRadius: BorderRadius.circular(30.0),
         ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.work),
-          label: 'Projects',
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            IconButton(
+              icon: Icon(Icons.info, color: Colors.white),
+              onPressed: () {
+                Navigator.pushNamed(context, '/');
+              },
+            ),
+            IconButton(
+              icon: Icon(Icons.phone, color: Colors.white),
+              onPressed: () {
+                Navigator.pushNamed(context, '/third');
+              },
+            ),
+            IconButton(
+              icon: Icon(Icons.work,
+                  color: const Color.fromARGB(255, 255, 255, 255)),
+              onPressed: () {
+                Navigator.pushNamed(context, '/second');
+              },
+            ),
+          ],
         ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.contact_mail),
-          label: 'Contact',
-        ),
-      ],
-      onTap: (index) {
-        switch (index) {
-          case 0:
-            Navigator.pushNamed(context, '/');
-            break;
-          case 1:
-            Navigator.pushNamed(context, '/second');
-            break;
-          case 2:
-            Navigator.pushNamed(context, '/third');
-            break;
-        }
-      },
+      ),
     );
   }
 }
