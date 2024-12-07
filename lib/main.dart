85e28ea2f1481f5b21d1fcf5274c34d6aebdfd81
habibiezkrillh <zikrillahhabibie@gmail.com>
2024-12-07 09:11:20 +0800

    fix

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'screens/first_screen.dart';
 import 'screens/second_screen.dart';
 import 'screens/third_screen.dart';
-import 'screens/route_generator_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -15,24 +14,9 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       initialRoute: '/',
       routes: {
-        '/': (context) => RouteGeneratorScreen(),
+        '/': (context) => FirstScreen(),
         '/second': (context) => SecondScreen(),
         '/third': (context) => ThirdScreen(),
-      },
-      // Handle dynamic routes
-      onGenerateRoute: (settings) {
-        return MaterialPageRoute(
-          builder: (context) {
-            // You can parse settings.name here to handle dynamic routes
-            // For now, we'll just show a generic screen with the route info
-            return Scaffold(
-              appBar: AppBar(title: Text('Dynamic Route')),
-              body: Center(
-                child: Text('Route: ${settings.name}'),
-              ),
-            );
-          },
-        );
       },
     );
   }
